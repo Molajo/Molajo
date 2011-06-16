e852e5683f7c370a7d368e738c8860d415043335
Matt Thomas <matt@betweenbrain.com>
2011-06-16 19:49:41 -0400

    Flirting with disaster :)

--- administrator/templates/rt_missioncontrol_j16/overrides/com_weblinks/override.css.php
+++
@@ -1,7 +0,0 @@
-<?php header("Content-type: text/css"); 
-//this sets up the colors for the core missioncontrol template
-?>
-.mc-form-frame .width-60 {width:50%;}
-.mc-form-frame .width-40 {width:50%;}
-
-
