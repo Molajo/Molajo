092263c2d5d6cac25736c37dd0c675c6e1e30caa
Amy Stephen <AmyStephen@gmail.org>
2011-07-04 12:18:40 -0500

    Initialize

--- administrator/components/com_articles/sql/uninstall.mysql.utf8.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE IF EXISTS `#__articles`;
-DELETE FROM `#__assets` WHERE `name` LIKE 'com_articles%';
-DELETE FROM `#__configuration` WHERE `component_option` = 'com_articles';
-DELETE FROM `#__categories` WHERE `extension` = 'com_articleings';
--- administrator/templates/rt_missioncontrol_j16/overrides/com_users/injections-post.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-pq('form[name=adminForm] .fltrt .mc-panel-padding')->prepend(pq('fieldset#user-groups'));
-pq('form[name=adminForm] #sliders ')->wrapAll('<fieldset class="adminform" />');
