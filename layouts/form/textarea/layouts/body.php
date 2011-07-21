56b6509486511c4dfdb1051f5cc56f49ed7c8aee
Amy Stephen <AmyStephen@gmail.org>
2011-07-21 09:17:29 -0500

    Add *.bs to gitignore to ignore the file.bs files locally in folders so that git will take the empty folder

---
+++ layouts/form/textarea/layouts/body.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @package     Molajo
+ * @subpackage  Layout
+ * @copyright   Copyright (C) 2011 Amy Stephen, Cristina Solana. All rights reserved.
+ * @license     GNU General Public License Version 2, or later http://www.gnu.org/licenses/gpl.html
+ */
+defined('MOLAJO') or die; ?>
+<?php if ($this->row->label == "") {
+} else { ?>
+<label
+    class="hasTip"
+    <?php if ($this->row->id == "") { } else { echo ' for="'.htmlspecialchars($this->row->id, ENT_COMPAT, 'UTF-8').'"'; } ?>
+    <?php if ($this->row->description == "") { } else { echo ' name="'.htmlspecialchars(JText::_($this->row->description)).'"'; } ?>
+    <?php echo JText::_(($this->row->label), ENT_COMPAT, 'UTF-8'); ?>
+    >
+<?php } ?>
+    <textarea
+       <?php if ($this->row->id == "") { } else { echo ' id="'.htmlspecialchars($this->row->id, ENT_COMPAT, 'UTF-8').'"'; } ?>
+       <?php if ($this->row->name == "") { } else { echo ' name="'.$this->row->name.'"'; } ?>
+       <?php if ($this->row->required === true) { } else { $this->row->class .= ' required'; } ?>
+       <?php if ($this->row->class == "") { } else { echo ' class="'.htmlspecialchars($this->row->class, ENT_COMPAT, 'UTF-8').'"'; } ?>
+       <?php if ($this->row->disabled === false) { } else { echo ' disabled="disabled"'; } ?>
+       <?php if ((int) $this->row->maxlength == 0) { } else { echo ' maxlength="'.(int) $this->row->maxlength.'"'; } ?>
+       <?php if ($this->row->name == "") { } else { echo ' name="'.$this->row->name.'"'; } ?>
+       <?php if ($this->row->onchange == "") { } else { echo ' onchange="'.(string) $this->row->onchange.'"'; } ?>
+       <?php if ($this->row->readonly === true) { echo ' readonly="readonly"'; } ?>
+       <?php if ((int) $this->row->size == 0) { } else { echo ' size="'.(int) $this->row->size.'"'; } ?>
+       <?php if ((int) $this->row->columns == 0) { } else { echo ' columns="'.(int) $this->row->columns.'"'; } ?>
+       <?php if ((int) $this->row->rows == 0) { } else { echo ' size="'.(int) $this->row->rows.'"'; } ?>
+    >
+       <?php echo htmlspecialchars($this->row->value, ENT_COMPAT, 'UTF-8'); ?>
+    </textarea>
+<?php if ($this->row->label == "") {
+} else { ?>
+</label>
+<?php }
