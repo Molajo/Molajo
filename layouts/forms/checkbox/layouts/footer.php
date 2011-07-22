022f3f78a166b3e4a0622723ab36e7bcf725f2c9
Cristina <cs@nightshiftcreative.com>
2011-07-22 10:26:45 -0400

    fixing php booboo

--- layouts/forms/checkbox/layouts/footer.php
+++ layouts/forms/checkbox/layouts/footer.php
@@ -6,6 +6,6 @@
  * @license     GNU General Public License Version 2, or later http://www.gnu.org/licenses/gpl.html
  */
 defined('MOLAJO') or die; ?>
-<?php if ($this->row->grouped === true) {
+<?php if ($this->row->grouped === true) { ?>
 </li>
 <?php }
