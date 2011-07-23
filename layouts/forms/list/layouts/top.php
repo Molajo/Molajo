643545db8aed1cda949b1f381e4592e71c056ef4
Cristina Solana <cs@nightshiftcreative.com>
2011-07-23 00:10:43 -0400

    mod list field

--- layouts/forms/list/layouts/top.php
+++ layouts/forms/list/layouts/top.php
@@ -6,4 +6,4 @@
  * @license     GNU General Public License Version 2, or later http://www.gnu.org/licenses/gpl.html
  */
 defined('MOLAJO') or die; ?>
-<ul class="latest-news<?php //echo $this->params->get('page_class_suffix', ''); ?>">
+<ul class="<?php //echo $this->params->get('page_class_suffix', ''); ?>">
