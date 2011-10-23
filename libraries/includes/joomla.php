efbee7e2c303044c27fdd9c5cc7e8a6c6f540de0
Babs Gosgens <babsgosgens@gmail.com>
2011-10-23 12:35:53 +0200

    Missing slash in path

--- libraries/includes/joomla.php
+++ libraries/includes/joomla.php
@@ -238,5 +238,5 @@ $filehelper->requireClassFile(JOOMLA_LIBRARY.'/utilities/date.php', 'JDate');
 /**
  *  PHPMailer
  */
-$filehelper->requireClassFile(LIBRARIES.'jplatform/phpmailer/phpmailer.php', 'PHPMailer');
-
+$filehelper->requireClassFile(LIBRARIES.'/jplatform/phpmailer/phpmailer.php', 'PHPMailer');
+var_dump(LIBRARIES.'/jplatform/phpmailer/phpmailer.php');
