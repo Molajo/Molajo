753e11e95655105c98dc46135a368c3d742a8286
Amy Stephen <AmyStephen@gmail.org>
2011-10-27 20:37:18 -0500

    Social Bookmarks Layout

--- layouts/extensions/social_bookmarks/js/load.js
+++ layouts/extensions/social_bookmarks/js/load.js
@@ -2,15 +2,37 @@
 // Load Social Bookmarks Buttons
 // Copyright (C) 2011 Amy Stephen. All rights reserved.
 // License GNU General Public License version 2 or later http://www.gnu.org/licenses/gpl.html
-$.ajax({
-	url: "reload-content.html",
-	cache: false,
-	success: function(html){
-		$("#results").html(html);
-    	twttr.widgets.load();
-    	try{
-			FB.XFBML.parse();
-    	}catch(ex){}
-  	}
+
+// Twitter
+$.get('index.php?option=com_layouts&tmpl=component&view=popup&layout=dummy', function(html)
+{
+	var $response = $( html );
+
+	// if there is an empty response do nothing
+	if( $response.length < 1 ) return;
+
+	// create the twitter buttons
+	$response.find('a.twitter-share-button').each(function()
+	{
+		var tweet_button = new twttr.TweetButton( $( this ).get( 0 ) );
+		tweet_button.render();
+	});
 });
+
+
+// Facebook
+$.get('index.php?option=com_layouts&tmpl=component&view=popup&layout=dummy', function(html)
+{
+	var $response = $( html );
+
+	// if there is an empty response do nothing
+	if( $response.length < 1 ) return;
+
+	// create the facebook buttons
+	$response.find('.btn_fb').each(function()
+	{
+		FB.XFBML.parse( this );
+	});
+});
+
 
--- layouts/extensions/social_bookmarks/layouts/custom.php
+++ layouts/extensions/social_bookmarks/layouts/custom.php
@@ -5,4 +5,9 @@
  * @copyright   Copyright (C) 2011 Amy Stephen. All rights reserved.
  * @license     GNU General Public License Version 2, or later http://www.gnu.org/licenses/gpl.html
  */
-defined('MOLAJO') or die;
+defined('MOLAJO') or die; ?>
+
+<a href="http://twitter.com/share" class="twitter-share-button">Tweet</a>
+<div class="share_button btn_fb">
+	<fb:like layout="button_count" font="tahoma"></fb:like>
+</div>
