77ab2b5afce51c73521460157d5d45287d6648ca
Amy Stephen <AmyStephen@gmail.org>
2011-11-19 09:41:42 -0600

    Remove extra JS

---
+++ extensions/templates/dash/js/scripts.js
@@ -0,0 +1,43 @@
+$(function(){
+
+				// Accordion
+				$('.accordion .head').click(function() {
+					$(this).next().toggle('slow');
+					return false;
+				}).next().hide();
+				
+				$( "#accordion" )
+					.accordion({
+						header: "> div > h2"
+					})
+					.sortable({
+						axis: "y",
+						handle: "h2",
+						stop: function() {
+							stop = true;
+					}
+				});
+				
+				$( "#accordion" ).accordion({
+					fillSpace: true,
+					autoHeight: false,
+					navigation: true,
+					collapsible: true
+				});
+				
+				// Tabs
+				$('#tabs').tabs();
+				
+				// Toggle editor action buttons
+				// will need to add check for actual state
+				$('.editor .actions li a').click(function () {
+				      $(this).toggleClass("enabled");
+				});
+				
+				//Search field hide and slide
+				$('span.lp_search').hide();
+				$('a.icon_search').click(function () {
+					//smoothen toggle, possibly add animate function
+				      $('span.lp_search').slideToggle('fast');
+				});
+});
