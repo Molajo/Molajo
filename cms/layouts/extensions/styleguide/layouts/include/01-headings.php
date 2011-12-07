36e789bb456a558fa4d2c67a0b5306bcb511897b
Amy Stephen <AmyStephen@gmail.org>
2011-12-07 14:19:30 -0600

    Aloha demo

---
+++ cms/layouts/extensions/styleguide/layouts/include/01-headings.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package     Molajo
+ * @subpackage  Style Guide
+ * @copyright   Copyright (C) 2012 Amy Stephen. All rights reserved.
+ * @license     GNU General Public License Version 2, or later http://www.gnu.org/licenses/gpl.html
+ */
+defined('MOLAJO') or die; ?>
+
+<?php echo MolajoTextHelper::_('MOLAJO_STYLEGUIDE_HEADINGS'); ?>
+
+<h1><?php echo MolajoTextHelper::_('MOLAJO_STYLEGUIDE_HEADING1'); ?></h1>
+
+<h2><?php echo MolajoTextHelper::_('MOLAJO_STYLEGUIDE_HEADING2'); ?></h2>
+
+<h3><?php echo MolajoTextHelper::_('MOLAJO_STYLEGUIDE_HEADING3'); ?></h3>
+
+<h4><?php echo MolajoTextHelper::_('MOLAJO_STYLEGUIDE_HEADING4'); ?></h4>
+
+<h5><?php echo MolajoTextHelper::_('MOLAJO_STYLEGUIDE_HEADING5'); ?></h5>
+
+<h6><?php echo MolajoTextHelper::_('MOLAJO_STYLEGUIDE_HEADING6'); ?></h6>
+
---
+++ cms/layouts/extensions/styleguide/layouts/include/04-blockquotes.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package     Molajo
+ * @subpackage  Style Guide
+ * @copyright   Copyright (C) 2012 Amy Stephen. All rights reserved.
+ * @license     GNU General Public License Version 2, or later http://www.gnu.org/licenses/gpl.html
+ */
+defined('MOLAJO') or die; ?>
+
+<h4><?php echo MolajoTextHelper::_('MOLAJO_STYLEGUIDE_BLOCKQUOTE'); ?></h4>
+
+<blockquote>
+ <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut laborum.</p>
+</blockquote>
