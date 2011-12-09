13d5b37863e906cdda5cd450d1b41850356505b8
Amy Stephen <AmyStephen@gmail.org>
2011-12-08 23:45:19 -0600

    Article.xml test

--- cms/templates/maji/index.php
+++ cms/templates/maji/index.php
@@ -6,8 +6,6 @@
  * @license     GNU General Public License Version 2, or later http://www.gnu.org/licenses/gpl.html
  */
 defined('MOLAJO') or die;
-
-
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo MOLAJO_BASE_FOLDER; ?>"
