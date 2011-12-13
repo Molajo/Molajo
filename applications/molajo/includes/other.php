8094003fafd83b56e89c119f0c56d1dd272e051c
Amy Stephen <AmyStephen@gmail.org>
2011-12-13 11:15:02 -0600

    Maji for UI examples

--- applications/molajo/includes/other.php
+++ applications/molajo/includes/other.php
@@ -18,10 +18,13 @@ $filehelper->requireClassFile(PLATFORM . '/jplatform/simplepie/simplepie.php', '
 require_once TWIG . '/Autoloader.php';
 Twig_Autoloader::register();
 
-$loader = new Twig_Loader_String();
-$twig = new Twig_Environment($loader);
+//$loader = new Twig_Loader_String();
+//$twig = new Twig_Environment($loader);
 
-require_once dirname(__FILE__).'/../lib/Github/Autoloader.php';
+/**
+ *  Github 
+ */
+require_once GITHUB . '/Autoloader.php';
 Github_Autoloader::register();
 
 /** Twig Autoload */
@@ -39,3 +42,56 @@ Github_Autoloader::register();
 //$classLoader = new \Doctrine\Common\ClassLoader('Doctrine');
 //var_dump($classLoader);
 //$classLoader->register();
+
+//$github = new Github_Client();
+//Search for users by username
+
+//$users = $github->getUserApi()->search('amy');
+//echo '<pre>';var_dump($users);echo '</pre>';
+//die;
+//  Get information about a user
+
+//$user = $github->getUserApi()->show('ornicar');
+
+//Update user informations
+
+//    $github->getUserApi()->update('ornicar', array('location' => 'France', 'blog' => 'http://diem-project.org/blog'));
+
+// Returns an array of information about the user.
+//Get users that a specific user is following
+
+//    $users = $github->getUserApi()->getFollowing('ornicar');
+//    $users = $github->getUserApi()->getFollowers('ornicar');
+
+//Make the authenticated user follow a user. Requires authentication.
+
+//    $github->getUserApi()->follow('symfony');
+/**
+Returns an array of followed users.
+Unfollow a user
+
+Make the authenticated user unfollow a user. Requires authentication.
+
+    $github->getUserApi()->unFollow('symfony');
+
+Returns an array of followed users.
+Get repos that a specific user is watching
+
+    $users = $github->getUserApi()->getWatchedRepos('ornicar');
+
+Returns an array of watched repos.
+Get the authenticated user emails
+
+    $emails = $github->getUserApi()->getEmails();
+
+Returns an array of the authenticated user emails. Requires authentication.
+Add an email to the authenticated user
+
+    $github->getUserApi()->addEmail('my-email@provider.org');
+
+Returns an array of the authenticated user emails. Requires authentication.
+Remove an email from the authenticated user
+
+    $github->getUserApi()->removeEmail('my-email@provider.org');
+ */
+
