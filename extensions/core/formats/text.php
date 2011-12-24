f3ae813be64d553473dacbf1c479bb7296958cfc
Amy Stephen <AmyStephen@gmail.org>
2011-12-24 00:25:45 -0600

    Additional Renderer Shells

---
+++ extensions/core/formats/text.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * @package     Molajo
+ * @subpackage  Text
+ * @copyright   Copyright (C) 2012 Amy Stephen. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+defined('MOLAJO') or die;
+
+/**
+ * Render Text Format
+ *
+ * @package     Molajo
+ * @subpackage  Text
+ * @since       1.0
+ */
+class MolajoTextFormat
+{
+    /**
+     *  Config
+     *
+     * @var array
+     * @since 1.0
+     */
+    protected $config = null;
+
+    /**
+     *  Message
+     *
+     * @var string
+     * @since 1.0
+     */
+    protected $message = null;
+
+    /**
+     *  Template folder name
+     *
+     * @var string
+     * @since 1.0
+     */
+    protected $template = null;
+
+    /**
+     *  Page include file
+     *
+     * @var string
+     * @since 1.0
+     */
+    protected $page = null;
+
+    /**
+     *  Layout include file
+     *
+     * @var string
+     * @since 1.0
+     */
+    protected $layout = null;
+
+    /**
+     *  Wrap for Layout
+     *
+     * @var string
+     * @since 1.0
+     */
+    protected $wrap = null;
+
+    /**
+     *  Template Parameters
+     *
+     * @var string
+     * @since 1.0
+     */
+    protected $parameters = null;
+
+    /**
+     * Array of buffered output
+     *
+     * @var    mixed (depends on the renderer)
+     */
+    protected $_buffer = null;
+
+    /**
+     * __construct
+     *
+     * Class constructor.
+     *
+     * @param   null    $request
+     * @param   null    $page
+     *
+     * @return boolean
+     *
+     * @since  1.0
+     */
+    public function __construct($config = array())
+    {
+//        echo '<pre>';
+//        var_dump($config);
+//        '</pre>';
+
+        /** set class properties */
+        $this->config = $config;
+        $this->message = $config->message;
+        $this->template = $config->template;
+        $this->page = $config->page;
+        $this->layout = $config->layout;
+        $this->wrap = $config->wrap;
+
+        /** Request */
+        $this->_renderTemplate();
+    }
+
+    /**
+     * Render the Template
+     *
+     * @return  object
+     * @since  1.0
+     */
+    protected function _renderTemplate()
+    {
+    }
+}
