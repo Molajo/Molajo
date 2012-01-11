1653c626068e3089144879f9a7d48a41d1adce8a
Babs Gosgens <babsgosgens@gmail.com>
2012-01-11 20:46:14 +0100

    Adding foundation based base markup

---
+++ foundation/snippets/toolbar.php
@@ -0,0 +1,12 @@
+				<div class="cf">
+					<ul class="actions cf unstyled">
+						<!-- Editor View Buttons -->
+						<li><a href="#" class="nice small radius blue button new"><span>New</span></a></li>
+						<li><a href="#" class="nice small radius white button edit"><span>Edit</span></a></li>
+						<li><a href="#" class="nice small radius white button danger delete"><span>Delete</span></a></li>
+						<li><a href="#" class="nice small radius white button danger trash"><span>Empty</span></a></li>
+						<li><a href="#" class="nice small radius white button spam"><span>Spam</span></a></li>
+						<li><a href="#" class="nice small radius white button archive"><span>Archive</span></a></li>
+						<li><a href="#" class="nice small radius white button info checkin"><span>Checkin</span></a></li>
+					</ul>
+				</div>
