5fcc3c461295031363074acadd26f9a6b4615a46
Amy Stephen <AmyStephen@gmail.org>
2012-02-15 16:23:06 -0600

    Modules and DB changes for Grids

--- extensions/views/templates/grid/views/custom.php
+++ extensions/views/templates/grid/views/custom.php
@@ -7,8 +7,8 @@
  */
 defined('MOLAJO') or die; ?>
 <include:module name=admin-toolbar template=admin-toolbar wrap=header />
-<include:module name=admin-submenu template=admin-submenu wrap=menu />
-<include:module name=grid-filters template=grid-filters wrap=menu />
+<include:module name=admin-submenu template=admin-submenu wrap=nav />
+<include:module name=grid-filters template=grid-filters wrap=nav />
 <include:module name=grid-table template=grid-table wrap=section />
-<include:module name=grid-pagination template=grid-pagination wrap=menu />
-<include:module name=grid-batch template=grid-batch wrap=menu />
+<include:module name=grid-pagination template=grid-pagination wrap=nav />
+<include:module name=grid-batch template=grid-batch wrap=nav />
