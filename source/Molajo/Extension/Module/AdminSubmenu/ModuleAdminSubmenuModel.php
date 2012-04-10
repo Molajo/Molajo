5b009fcbaf81c34173d5a8d32db26fdc690163ba
Amy Stephen <AmyStephen@gmail.org>
2012-04-10 18:07:36 -0500

    Source folder

---
+++ source/Molajo/Extension/Module/AdminSubmenu/ModuleAdminSubmenuModel.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @package   Molajo
+ * @copyright 2012 Amy Stephen. All rights reserved.
+ * @license   GNU General Public License Version 2, or later http://www.gnu.org/licenses/gpl.html
+ */
+namespace Molajo\Extension\Module;
+
+use Molajo\Application\MVC\Model\DisplayModel;
+use Molajo\Application\Services;
+
+defined('MOLAJO') or die;
+
+/**
+ * ModuleAdminSubmenuModel
+ *
+ * @package   Molajo
+ * @subpackage  Model
+ * @since       1.0
+ */
+Class ModuleAdminSubmenuModel extends DisplayModel
+{
+    /**
+     * __construct
+     *
+     * Constructor.
+     *
+     * @param  $config
+     * @since  1.0
+     */
+    public function __construct($table = null, $id = null, $path = null)
+    {
+        $this->name = get_class($this);
+
+        return parent::__construct($table, $id, $path);
+    }
+
+    /**
+     * getData
+     *
+     * @return    array
+     *
+     * @since    1.0
+     */
+    public function getData()
+    {
+        $links =
+            Services::Parameter()
+                ->get('submenu_items', 'Request');
+
+        $linksArray = explode(',', $links);
+
+        $this->data = array();
+        foreach ($linksArray as $link) {
+            $this->data[] = $link;
+        }
+
+        return $this->data;
+    }
+}
---
+++ source/Site/1/configuration.php
@@ -0,0 +1,51 @@
+<?php
+Class SiteConfiguration
+{
+    /* Site Settings */
+    public $site_name = 'Molajo';
+
+    /* JDatabaseFactory Settings */
+    public $jdatabasefactory_dbtype = 'mysqli';
+    public $jdatabasefactory_host = 'localhost';
+    public $jdatabasefactory_user = 'root';
+    public $jdatabasefactory_password = 'root';
+    public $jdatabasefactory_db = 'molajo';
+    public $jdatabasefactory_dbprefix = 'molajo_';
+    public $jdatabasefactory_namespace = 'Joomla\\database\\JDatabaseFactory';
+
+    /** disable html filters */
+    public $disable_filter_for_groups = '4';
+
+    /* Server Settings */
+    public $secret = 'FBVtggIk5lAzEU9H';
+    public $gzip = '0';
+    public $error_reporting = '-1';
+    public $cache_path = 'cache';
+    public $logs_path = 'logs';
+    public $temp_path = 'temp';
+    public $temp_url = 'temp';
+    public $media_path = 'media';
+    public $media_url = 'media';
+
+    /* Session settings */
+    public $lifetime = '15';
+    public $session_handler = 'database';
+    public $cookie_domain = '';
+    public $cookie_path = '';
+
+    /* Mail Settings */
+    public $disable_sending = true;
+    public $only_deliver_to = 'AmyStephen@gmail.com';
+    public $mailer = 'mail';
+    public $mail_from_email_address = '';
+    public $mail_from_name = '';
+    public $send_mail = '/usr/sbin/send_mail';
+    public $smtpauth = '0';
+    public $smtpuser = '';
+    public $smtppass = '';
+    public $smtphost = 'localhost';
+    public $mail_class = 'JMail';
+
+    /* Debug */
+    public $debug = '1';
+}
