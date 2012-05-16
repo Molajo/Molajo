c2648111c46afe9b763aedc2823b489a444dc5f7
AmyStephen <AmyStephen@gmail.com>
2012-05-16 05:57:08 -0500

    Responsify breakpoints example

---
+++ Extension/Theme/responsify/View/Page/Default/index.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @package     Molajo
+ * @subpackage  Theme
+ * @copyright   2012 Amy Stephen. All rights reserved.
+ * @license     GNU General Public License Version 2, or later http://www.gnu.org/licenses/gpl.html
+ *
+ */
+//defined('MOLAJO') or die; ?>
+<!doctype html>
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+
+	<title>Responsify.it - A responsive template generator</title>
+
+	<link rel="shortcut icon" href="/favicon.ico">
+	<link rel="stylesheet" href="style.css">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.5.3/modernizr.min.js"></script>
+
+</head>
+
+<body class="cf">
+
+<header class="span16">
+	<div class="inner cf">
+		<h1 class="alt span11">responsify</h1>
+		<nav class="span5 col">
+			<ul class="cf">
+				<li><a class="alt" href="#">nav item</a></li>
+				<li><a class="alt" href="#">nav item</a></li>
+				<li><a class="alt" href="#">nav item</a></li>
+			</ul>
+		</nav>
+	</div>
+</header>
+
+<div id="container" class="cf">
+
+	<article class="span11">
+		<section>
+			<h1>section h1</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec.</p>
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra.</p>
+		</section>
+		<section>
+			<h2>section h2</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra.</p>
+		</section>
+		<section>
+			<h2>section h2</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor.</p>
+		</section>
+	</article>
+
+	<aside class="span5 col">
+		<h2>aside h2</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor.</p>
+
+		<h2>aside h2</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor.</p>
+	</aside>
+
+</div> <!--! end of #container -->
+
+<footer>
+
+</footer>
+
+</body>
+</html>
