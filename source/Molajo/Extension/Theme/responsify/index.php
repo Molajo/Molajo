436e2a7e344a8609157ac2978236683df5bed40a
Amy Stephen <AmyStephen@gmail.org>
2012-05-16 05:57:08 -0500

    Responsify breakpoints example

---
+++ source/Molajo/Extension/Theme/responsify/index.php
@@ -0,0 +1,11 @@
+<?php
+use Molajo\Service\Services;
+
+/**
+ * @package     Molajo
+ * @subpackage  Responsify
+ * @copyright   2012 Amy Stephen. All rights reserved.
+ * @license     GNU General Public License Version 2, or later http://www.gnu.org/licenses/gpl.html
+ */
+defined('MOLAJO') or die;
+include Services::Registry()->get('PageView', 'path_include');
