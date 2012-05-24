a0c7f104c9cbbe2467c633e97c84dca58a432cb7
AmyStephen <AmyStephen@gmail.com>
2012-05-24 11:55:23 -0500

    Remove unnecessary second catalog join for content

---
+++ Extension/View/Template/Adminsidemenu/View/Footer.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package   Molajo
+ * @subpackage  Views
+ * @copyright 2012 Amy Stephen. All rights reserved.
+ * @license   GNU General Public License Version 2, or later http://www.gnu.org/licenses/gpl.html
+ */
+defined('MOLAJO') or die; ?>
+<footer>
+	<?php echo $this->row->start_publishing_datetime; ?>
+</footer>
