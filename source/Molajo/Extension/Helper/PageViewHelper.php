520f58791b81eb69f2f3588983455475150e9a5d
Amy Stephen <AmyStephen@gmail.org>
2012-05-30 22:39:26 -0500

    PageViewHelper with new Model

--- source/Molajo/Extension/Helper/PageViewHelper.php
+++ source/Molajo/Extension/Helper/PageViewHelper.php
@@ -20,162 +20,163 @@ defined('MOLAJO') or die;
  */
 Class PageViewHelper
 {
-    /**
-     * Static instance
-     *
-     * @var    object
-     * @since  1.0
-     */
-    protected static $instance;
+	/**
+	 * Static instance
+	 *
+	 * @var    object
+	 * @since  1.0
+	 */
+	protected static $instance;
 
-    /**
-     * getInstance
-     *
-     * @static
-     * @return bool|object
-     * @since  1.0
-     */
-    public static function getInstance()
-    {
-        if (empty(self::$instance)) {
-            self::$instance = new PageViewHelper();
-        }
+	/**
+	 * getInstance
+	 *
+	 * @static
+	 * @return bool|object
+	 * @since  1.0
+	 */
+	public static function getInstance()
+	{
+		if (empty(self::$instance)) {
+			self::$instance = new PageViewHelper();
+		}
 
-        return self::$instance;
-    }
+		return self::$instance;
+	}
 
-    /**
-     * get
-     *
-     * Get requested page_view data
-     *
-     * @return boolean
-     * @since   1.0
-     */
-    public function get($page_view_id = 0)
-    {
+	/**
+	 * Get requested page_view data
+	 *
+	 * @param int $page_view_id
+	 *
+	 * @return boolean
+	 * @since   1.0
+	 */
+	public function get($page_view_id = 0)
+	{
+		Services::Registry()->set('Parameters', 'page_view_id', (int)$page_view_id);
 
-        Services::Registry()->set('Parameters', 'page_view_id', (int) $page_view_id);
+		$node = Helpers::Extension()->getExtensionNode((int)$page_view_id);
 
-        $node = Helpers::Extension()->getExtensionNode((int) $page_view_id);
+		Services::Registry()->set('Parameters', 'page_view_path_node', $node);
 
-        Services::Registry()->set('Parameters', 'page_view_path_node', $node);
+		Services::Registry()->set('Parameters', 'page_view_path', $this->getPath($node));
+		Services::Registry()->set('Parameters', 'page_view_path_include',
+			$this->getPath($node) . '/index.php');
+		Services::Registry()->set('Parameters', 'page_view_path_url', $this->getPathURL($node));
 
-        Services::Registry()->set('Parameters', 'page_view_path', $this->getPath($node));
-        Services::Registry()->set('Parameters', 'page_view_path_include',
-            $this->getPath($node) . '/index.php');
-        Services::Registry()->set('Parameters', 'page_view_path_url', $this->getPathURL($node));
+		/** Retrieve the query results */
+		$item = Helpers::Extension()->get($page_view_id, 'PageViews', 'Table');
 
-        /** Retrieve the query results */
-        $row = Helpers::Extension()->get($page_view_id, 'PageViews', 'Table');
+		/** 500: not found */
+		if (count($item) == 0) {
 
-        /** 500: not found */
-        if (count($row) == 0) {
+			/** System Default */
+			$page_view_id = Helpers::Extension()->getInstanceID(CATALOG_TYPE_EXTENSION_PAGE_VIEW, 'Default');
 
-            /** System Default */
-            $page_view_id = Helpers::Extension()->getInstanceID(CATALOG_TYPE_EXTENSION_PAGE_VIEW, 'Default');
+			/** System default */
+			Services::Registry()->set('Parameters', 'page_view_id', (int)$page_view_id);
 
-            /** System default */
-            Services::Registry()->set('Parameters', 'page_view_id', (int) $page_view_id);
+			$node = Helpers::Extension()->getExtensionNode((int)$page_view_id);
 
-            $node = Helpers::Extension()->getExtensionNode((int) $page_view_id);
+			Services::Registry()->set('Parameters', 'page_view_path_node', $node);
 
-            Services::Registry()->set('Parameters', 'page_view_path_node', $node);
+			Services::Registry()->set('Parameters', 'page_view_path', $this->getPath($node));
+			Services::Registry()->set('Parameters', 'page_view_path_include',
+				$this->getPath($node) . '/index.php');
+			Services::Registry()->set('Parameters', 'page_view_path_url', $this->getPathURL($node));
 
-            Services::Registry()->set('Parameters', 'page_view_path', $this->getPath($node));
-            Services::Registry()->set('Parameters', 'page_view_path_include',
-                $this->getPath($node) . '/index.php');
-            Services::Registry()->set('Parameters', 'page_view_path_url', $this->getPathURL($node));
+			$item = Helpers::Extension()->get($page_view_id, 'PageView');
 
-            $row = Helpers::Extension()->get($page_view_id, 'PageView');
+			if (count($item) == 0) {
+				Services::Error()->set(500, 'View not found');
 
-            if (count($row) == 0) {
-                Services::Error()->set(500, 'View not found');
+				return false;
+			}
+		}
 
-                return false;
-            }
-        }
+		Services::Registry()->set('Parameters', 'page_view_title', $item->title);
+		Services::Registry()->set('Parameters', 'page_view_translation_of_id', (int)$item->translation_of_id);
+		Services::Registry()->set('Parameters', 'page_view_language', $item->language);
+		Services::Registry()->set('Parameters', 'page_view_view_group_id', $item->view_group_id);
+		Services::Registry()->set('Parameters', 'page_view_catalog_id', $item->catalog_id);
+		Services::Registry()->set('Parameters', 'page_view_catalog_type_id', (int)$item->catalog_type_id);
+		Services::Registry()->set('Parameters', 'page_view_catalog_type_title', $item->catalog_types_title);
 
-        Services::Registry()->set('Parameters', 'page_view_title', $row['title']);
-        Services::Registry()->set('Parameters', 'page_view_translation_of_id', (int) $row['translation_of_id']);
-        Services::Registry()->set('Parameters', 'page_view_language', $row['language']);
-        Services::Registry()->set('Parameters', 'page_view_view_group_id', $row['view_group_id']);
-        Services::Registry()->set('Parameters', 'page_view_catalog_id', $row['catalog_id']);
-        Services::Registry()->set('Parameters', 'page_view_catalog_type_id', (int) $row['catalog_type_id']);
-        Services::Registry()->set('Parameters', 'page_view_catalog_type_title', $row['catalog_types_title']);
+		return true;
+	}
 
-        return true;
-    }
+	/**
+	 * Return path for selected View
+	 *
+	 * Expects known path for Theme and Extension
+	 *
+	 * @param $node
+	 *
+	 * @return bool|string
+	 * @since  1.0
+	 */
+	public function getPath($node)
+	{
+		$plus = '/View/Page/' . ucfirst(strtolower($node));
 
-    /**
-     * getPath
-     *
-     * Return path for selected View
-     *
-     * Expects known path for Theme and Extension
-     *
-     * @param $node
-     * @return bool|string
-     */
-    public function getPath($node)
-    {
-        $plus = '/View/Page/' . ucfirst(strtolower($node));
+		/** 1. Theme */
+		if (file_exists(Services::Registry()->get('Parameters', 'theme_path') . $plus . '/Manifest.xml')) {
+			return Services::Registry()->get('Parameters', 'theme_path') . $plus;
+		}
 
-        /** 1. Theme */
-        if (file_exists(Services::Registry()->get('Parameters', 'theme_path') . $plus . '/Manifest.xml')) {
-            return Services::Registry()->get('Parameters', 'theme_path') . $plus;
-        }
+		/** 2. Extension */
+		if (file_exists(Services::Registry()->get('Parameters', 'extension_path') . $plus . '/Manifest.xml')) {
+			return Services::Registry()->get('Parameters', 'extension_path') . $plus;
+		}
 
-        /** 2. Extension */
-        if (file_exists(Services::Registry()->get('Parameters', 'extension_path') . $plus . '/Manifest.xml')) {
-            return Services::Registry()->get('Parameters', 'extension_path') . $plus;
-        }
+		/** 3. View */
+		if (file_exists(EXTENSIONS_VIEWS . '/Page/' . ucfirst(strtolower($node)) . '/Manifest.xml')) {
+			return EXTENSIONS_VIEWS . '/Page/' . ucfirst(strtolower($node));
+		}
 
-        /** 3. View */
-        if (file_exists(EXTENSIONS_VIEWS . '/Page/' . ucfirst(strtolower($node)) . '/Manifest.xml')) {
-            return EXTENSIONS_VIEWS . '/Page/' . ucfirst(strtolower($node));
-        }
+		/** 4. MVC */
+		if (file_exists(MVC . $plus . '/Manifest.xml')) {
+			return MVC . $plus;
+		}
 
-        /** 4. MVC */
-        if (file_exists(MVC . $plus . '/Manifest.xml')) {
-            return MVC . $plus;
-        }
+		return false;
+	}
 
-        return false;
-    }
+	/**
+	 * getURLPath
+	 *
+	 * Return URL path for selected Template View
+	 *
+	 * @param bool $node
+	 *
+	 * @return string
+	 * @since  1.0
+	 */
+	public function getPathURL($node = false)
+	{
+		$plus = '/View/Page/' . ucfirst(strtolower($node));
 
-    /**
-     * getURLPath
-     *
-     * Return URL path for selected Template View
-     *
-     * @return bool|string
-     * @since 1.0
-     */
-    public function getPathURL($node = false)
-    {
-        $plus = '/View/Page/' . ucfirst(strtolower($node));
+		/** 1. Theme */
+		if (file_exists(Services::Registry()->get('Parameters', 'theme_path') . $plus . '/Manifest.xml')) {
+			return Services::Registry()->get('Parameters', 'theme_path_url') . $plus;
+		}
 
-        /** 1. Theme */
-        if (file_exists(Services::Registry()->get('Parameters', 'theme_path') . $plus . '/Manifest.xml')) {
-            return Services::Registry()->get('Parameters', 'theme_path_url') . $plus;
-        }
+		/** 2. Extension */
+		if (file_exists(Services::Registry()->get('Parameters', 'extension_path') . $plus . '/Manifest.xml')) {
+			return Services::Registry()->get('Parameters', 'extension_path_url') . $plus;
+		}
 
-        /** 2. Extension */
-        if (file_exists(Services::Registry()->get('Parameters', 'extension_path') . $plus . '/Manifest.xml')) {
-            return Services::Registry()->get('Parameters', 'extension_path_url') . $plus;
-        }
+		/** 3. View */
+		if (file_exists(EXTENSIONS_VIEWS . '/Page/' . ucfirst(strtolower($node)) . '/Manifest.xml')) {
+			return EXTENSIONS_VIEWS_URL . '/Page/' . ucfirst(strtolower($node));
+		}
 
-        /** 3. View */
-        if (file_exists(EXTENSIONS_VIEWS . '/Page/' . ucfirst(strtolower($node)) . '/Manifest.xml')) {
-            return EXTENSIONS_VIEWS_URL . '/Page/' . ucfirst(strtolower($node));
-        }
+		/** 4. MVC */
+		if (file_exists(MVC . $plus . '/Manifest.xml')) {
+			return MVC_URL . $plus;
+		}
 
-        /** 4. MVC */
-        if (file_exists(MVC . $plus . '/Manifest.xml')) {
-            return MVC_URL . $plus;
-        }
-
-        return false;
-    }
+		return '';
+	}
 }
