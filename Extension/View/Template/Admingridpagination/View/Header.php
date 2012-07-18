7f548d6972e5938157022910fad6e18254cc6323
AmyStephen <AmyStephen@gmail.com>
2012-07-18 00:11:33 -0500

    Pagination

--- Extension/View/Template/Admingridpagination/View/Header.php
+++ Extension/View/Template/Admingridpagination/View/Header.php
@@ -1,10 +1,13 @@
 <?php
+use Molajo\Service\Services;
+
 /**
  * @package    Molajo
  * @copyright  2012 Amy Stephen. All rights reserved.
  * @license    GNU GPL v 2, or later and MIT, see License folder
  */
-defined('MOLAJO') or die; ?>
+defined('MOLAJO') or die;
+?>
 <ul class="pagination">
-    <li class="unavailable"><a href="">&laquo;</a></li>
+    <li class="unavailable"><a href="<?php echo $this->row->prev_link; ?>">&laquo;</a></li>
 
