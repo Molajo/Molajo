aa71f81e5f550e8c25188c140bbc3e4e4a5b76db
Amy Stephen <AmyStephen@gmail.org>
2012-07-20 16:43:35 -0500

    Dashboard shell/menu items

--- source/Molajo/Extension/Theme/Molajito/index.php
+++ source/Molajo/Extension/Theme/Molajito/index.php
@@ -4,7 +4,6 @@ use Molajo\Service\Services;
  * @package    Molajo
  * @copyright  2012 Amy Stephen. All rights reserved.
  * @license    GNU GPL v 2, or later and MIT, see License folder
- * todo: think about error/offline templates get rid of page
  */
 defined('MOLAJO') or die; ?>
 <include:head/>
@@ -17,10 +16,9 @@ defined('MOLAJO') or die; ?>
 <div class="stickywrapper">
 	<div class="row">
 		<div class="twelve columns">
-
-<?php if (file_exists(Services::Registry()->get('Parameters', 'page_view_path_include'))) {
-	include Services::Registry()->get('Parameters', 'page_view_path_include');
-} ?>
+		<?php if (file_exists(Services::Registry()->get('Parameters', 'page_view_path_include'))) {
+			include Services::Registry()->get('Parameters', 'page_view_path_include');
+		} ?>
 		</div>
 	</div>
 	<div class="row">
