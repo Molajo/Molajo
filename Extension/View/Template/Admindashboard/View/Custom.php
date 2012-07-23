ce985b6549d768f59867c60d3d92e094b30c228c
AmyStephen <AmyStephen@gmail.com>
2012-07-23 15:56:35 -0500

    Graph example for dashboard

--- Extension/View/Template/Admindashboard/View/Custom.php
+++ Extension/View/Template/Admindashboard/View/Custom.php
@@ -30,25 +30,25 @@ defined("MOLAJO") or die; ?>
 	<div class="twelve columns">
 
 		<div class="row">
-			<div class="four columns portlet1">
+			<div class="four columns ui-widget1">
 
-				<div id="articles-portlet" class="portlet">
-					<div class="portlet-header">
-						Recent Articles
+				<div id="articles-ui-widget" class="ui-widget">
+					<div class="ui-widget-header">
+						<h4>Graph of Something</h4>
 					</div>
-					<div class="portlet-content">
+					<div class="ui-widget-content">
 						<ul>
-							<li><a href="#">Name of this article</a></li>
-							<li><a href="#">Name of this article</a></li>
+							<li><a href="#">Snippet from Comment</a></li>
+							<li><a href="#">Snippet from Comment</a></li>
 						</ul>
 					</div>
 				</div>
 
-				<div id="comments-portlet" class="portlet">
-					<div class="portlet-header">
-						Recent Comments
+				<div id="comments-ui-widget" class="ui-widget">
+					<div class="ui-widget-header">
+						<h4>Recent Comments</h4>
 					</div>
-					<div class="portlet-content">
+					<div class="ui-widget-content">
 						<ul>
 							<li><a href="#">Snippet from Comment</a></li>
 							<li><a href="#">Snippet from Comment</a></li>
@@ -58,16 +58,22 @@ defined("MOLAJO") or die; ?>
 
 			</div>
 
-			<div class="eight columns portlet2">
-				<div id="media-portlet" class="portlet">
-					<div class="portlet-header">
-						<h5>Media</h5>
+			<div class="eight columns ui-widget2">
+				<div id="media-ui-widget" class="ui-widget">
+					<div class="ui-widget-header">
+						<h4>Graph of Something</h4>
 					</div>
-					<div class="portlet-content">
-						<ul>
-							<li><a href="#">Snippet from Comment</a></li>
-							<li><a href="#">Snippet from Comment</a></li>
-						</ul>
+					<div class="ui-widget-content">
+						<dl style="width: 300px">
+							<dt>2008</dt>
+							<dd><div id="data-one" class="bar" style="width: 60%">60%</div></dd>
+							<dt>2009</dt>
+							<dd><div id="data-two" class="bar" style="width: 80%">80%</div></dd>
+							<dt>2010</dt>
+							<dd><div id="data-three" class="bar" style="width: 64%">64%</div></dd>
+							<dt>2011</dt>
+							<dd><div id="data-four" class="bar" style="width: 97%">97%</div></dd>
+						</dl>
 					</div>
 				</div>
 			</div>
@@ -75,26 +81,23 @@ defined("MOLAJO") or die; ?>
 		</div>
 
 		<div class="row">
-			<div class="four columns portlet3">
-				<div id="activity-portlet" class="portlet">
-					<div class="portlet-header">
+			<div class="four columns ui-widget3">
+				<div id="activity-ui-widget" class="ui-widget">
+					<div class="ui-widget-header">
 						<h4>Recent Activity</h4>
 					</div>
-					<div class="portlet-content">
-						<ul>
-							<li><a href="#">Activity</a></li>
-							<li><a href="#">Activitye</a></li>
-						</ul>
+					<div class="ui-widget-content">
+
 					</div>
 				</div>
 			</div>
 
-			<div class="four columns portlet4">
-				<div id="portlet-online" class="portlet">
-					<div class="portlet-header">
+			<div class="four columns ui-widget4">
+				<div id="ui-widget-online" class="ui-widget">
+					<div class="ui-widget-header">
 						<h4>Users Online</h4>
 					</div>
-					<div class="portlet-content">
+					<div class="ui-widget-content">
 						<ul>
 							<li><a href="#">User 1</a></li>
 							<li><a href="#">User 2</a></li>
@@ -103,12 +106,12 @@ defined("MOLAJO") or die; ?>
 				</div>
 			</div>
 
-			<div class="four columns portlet5">
-				<div id="portlet-scheduled" class="portlet">
-					<div class="portlet-header">
+			<div class="four columns ui-widget5">
+				<div id="ui-widget-scheduled" class="ui-widget">
+					<div class="ui-widget-header">
 						<h4>Scheduled for Publication</h4>
 					</div>
-					<div class="portlet-content">
+					<div class="ui-widget-content">
 						<ul>
 							<li><a href="#">Content 2</a></li>
 							<li><a href="#">Content 2</a></li>
--- Extension/View/Template/Admingridview/View/Viewcontenttypes.php
+++ Extension/View/Template/Admingridview/View/Viewcontenttypes.php
@@ -8,16 +8,16 @@ use Molajo\Service\Services;
 defined('MOLAJO') or die;
 
 $action = Services::Registry()->get('Triggerdata', 'full_page_url'); ?>
-<h4><?echo Services::Language()->translate('Categorize'); ?></h4>
-<form action="<?php echo $action; ?>" method="post" name="Admingridviewcategories" id="Admingridviewcategories">
-	<p><?php echo Services::Language()->translate('Add or remove selected content from specified category(ies).'); ?></p>
+<h4><?echo Services::Language()->translate('Resource Content Types'); ?></h4>
+<form action="<?php echo $action; ?>" method="post" name="Admingridviewcontenttypes" id="Admingridviewcontenttypes">
+	<p><?php echo Services::Language()->translate('Create a new or remove an existing Content Type. Before removing a Content Type. all items in the Content Type must first be deleted or moved to a different Content Type using the Batch Utility.'); ?></p>
 	<div class="row">
 		<div class="five columns">
-			<include:template name=Formselectlist model=Triggerdata value=<?php echo 'listbatch_categories*'; ?>/>
+			<include:template name=Formselectlist model=Triggerdata value=<?php echo 'listview_contenttypes*'; ?>/>
 		</div>
 		<div class="three columns">
-			<input type="submit" class="submit button small" name="submit" id="batch-category-create" value="Add">
-			<input type="submit" class="submit button small" name="submit" id="batch-category-delete" value="Remove">
+			<input type="submit" class="submit button small" name="submit" id="view-contenttypes-create" value="Add">
+			<input type="submit" class="submit button small" name="submit" id="view-contenttypes-delete" value="Remove">
 		</div>
 		<div class="four columns">&nbsp;</div>
 	</div>
