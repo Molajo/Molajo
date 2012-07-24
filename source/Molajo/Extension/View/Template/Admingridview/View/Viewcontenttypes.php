8a072d59f1f67b5f09021cd2d60e0d96d7875eea
Amy Stephen <AmyStephen@gmail.org>
2012-07-24 08:49:17 -0500

    Minor changes

--- source/Molajo/Extension/View/Template/Admingridview/View/Viewcontenttypes.php
+++ source/Molajo/Extension/View/Template/Admingridview/View/Viewcontenttypes.php
@@ -8,17 +8,34 @@ use Molajo\Service\Services;
 defined('MOLAJO') or die;
 
 $action = Services::Registry()->get('Triggerdata', 'full_page_url'); ?>
-<h4><?echo Services::Language()->translate('Resource Content Types'); ?></h4>
-<form action="<?php echo $action; ?>" method="post" name="Admingridviewcontenttypes" id="Admingridviewcontenttypes">
-	<p><?php echo Services::Language()->translate('Create a new or remove an existing Content Type. Before removing a Content Type. all items in the Content Type must first be deleted or moved to a different Content Type using the Batch Utility.'); ?></p>
-	<div class="row">
-		<div class="five columns">
-			<include:template name=Formselectlist model=Triggerdata value=<?php echo 'listview_contenttypes*'; ?>/>
+<div class="row">
+	<div class="twelve columns">
+	<h4><?echo Services::Language()->translate('Content Types'); ?></h4>
+	<form action="<?php echo $action; ?>" method="post" name="Admingridviewcontenttypes" id="Admingridviewcontenttypes">
+		<div class="row">
+			<div class="eight columns">
+			<p>
+				<?php echo Services::Language()->translate('Create a Content Type to be used to store different data for items associated so associated.'); ?></p>
+			</p>
+				<input type="text" name="content_type" id="content_type">
+			</div>
+			<div class="four columns">
+				<input type="submit" class="submit button small" name="submit" id="view-contenttypes-create" value="Add">
+			</div>
 		</div>
-		<div class="three columns">
-			<input type="submit" class="submit button small" name="submit" id="view-contenttypes-create" value="Add">
-			<input type="submit" class="submit button small" name="submit" id="view-contenttypes-delete" value="Remove">
+		<div class="row">
+			<div class="eight columns">
+			<p><?php echo Services::Language()->translate('Remove an existing Content Type. Note: items must first be deleted or moved to a different Content Type using the Batch Utility.'); ?></p>
+				<select name="content_types" class="inputbox">
+					<option value="">No selection</option>
+					<option value="20">Content Type 1</option>
+					<option value="21">Content Type 2</option>
+				</select>
+			</div>
+			<div class="four columns">
+				<input type="submit" class="submit button small" name="submit" id="view-contenttypes-delete" value="Remove">
+			</div>
 		</div>
-		<div class="four columns">&nbsp;</div>
+	</form>
 	</div>
-</form>
+</div>
