015ff600380be57d080267c23d2819323d4418f0
Babs Gosgens <babs@crossinghippos.nl>
2012-07-27 18:41:40 +0200

    Code full image path for now

--- source/Molajo/Extension/Theme/Mai/View/Template/Adminheader/View/Body.php
+++ source/Molajo/Extension/Theme/Mai/View/Template/Adminheader/View/Body.php
@@ -15,7 +15,6 @@ if ($title == '') {
 }
 $homeURL = Services::Registry()->get('Configuration', 'application_base_url');
 $resourceURL = Services::Registry()->get('Triggerdata', 'full_page_url');
-var_dump($resourceURL);
 ?>
 		<div>
 			<h1><a href="index.php"><span><strong>Molajo</strong> Admin Interface</span></a></h1>
@@ -30,7 +29,7 @@ var_dump($resourceURL);
 							</fieldset>
 						</form>
 					</dd
-					><dt class="user"><a href="#user"><img src="media/smile.png" alt="" width="40" height="40" /><span>Babs G&ouml;sgens</span></a></dt
+					><dt class="user"><a href="#user"><img src="/source/Molajo/Extension/Theme/Mai/Images/smile.png" alt="" width="40" height="40" /><span>Babs G&ouml;sgens</span></a></dt
 					><dd id="user">
 						<a href="#" class="dismiss"><i>g</i><span>Close</span></a>
 						<ul>
@@ -43,25 +42,8 @@ var_dump($resourceURL);
 					><dt class="last"><a href="#settings"><i>a</i><span>Settings</span></a></dt
 					><dd id="settings">
 						<a href="#" class="dismiss"><i>g</i><span>Close</span></a>
+						<include:template name=Adminnavigationbar/>
 					Settings</dd>
 				</dl>
 			</nav>
 		</div>
-
-<!-- <div class="row header">
-	<div class="twelve columns">
-		<div class="row topmenu topmenu-text">
-			<div class="three columns">
-				<span>Molajo</span>
-			</div>
-			<div class="nine columns">
-				<include:template name=Adminnavigationbar/>
-			</div>
-		</div>
-		<div class="row heading heading-text">
-			<div class="twelve columns">
-				<h1><?php echo $title; ?></h1>
-			</div>
-		</div>
-	</div>
-</div> -->
