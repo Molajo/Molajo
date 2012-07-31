0f097c9c9041083be9e693ae81ebb7e54af85a4c
Amy Stephen <AmyStephen@gmail.org>
2012-07-31 15:03:53 -0500

    DB updates for Portlets

--- source/Molajo/Extension/View/Template/Admindashboard/View/Custom.php
+++ source/Molajo/Extension/View/Template/Admindashboard/View/Custom.php
@@ -29,29 +29,29 @@ defined("MOLAJO") or die; ?>
 <div wrap_class="row">
 	<div wrap_class="twelve columns">
 		<div wrap_class="row ui-portlet1">
-			<include:template name=Widgetgallery wrap=div wrap_id=widget1 wrap_class=ui-portlet,three,columns/>
-			<include:template name=Widgetlist wrap=div wrap_id=widget2 wrap_class=ui-portlet,three,columns/>
-			<include:template name=Widgettext wrap=div wrap_id=widget3 wrap_class=ui-portlet,three,columns/>
+			<include:template name=Widgetgallery wrap=div wrap_id=widget1 wrap_class=ui-portlet,four,columns/>
+			<include:template name=Widgetlist wrap=div wrap_id=widget2 wrap_class=ui-portlet,four,columns/>
+			<include:template name=Widgettext wrap=div wrap_id=widget3 wrap_class=ui-portlet,four,columns/>
 		</div>
 		<div wrap_class="row ui-portlet2">
-			<include:template name=Widgetaudio wrap=div wrap_id=widget4 wrap_class=ui-portlet,three,columns/>
-
-			<include:template name=Widgetmap wrap=div wrap_id=widget6 wrap_class=ui-portlet,three,columns/>
+			<include:template name=Widgetaudio wrap=div wrap_id=widget4 wrap_class=ui-portlet,four,columns/>
+			<include:template name=Widgetvideo wrap=div wrap_id=widget5 wrap_class=ui-portlet,four,columns/>
+			<include:template name=Widgetmap wrap=div wrap_id=widget6 wrap_class=ui-portlet,four,columns/>
 		</div>
 		<div wrap_class="row ui-portlet3">
-			<include:template name=Widgetgraph wrap=div wrap_id=widget7 wrap_class=ui-portlet,three,columns/>
-			<include:template name=Widgetquicklinks wrap=div wrap_id=widget8 wrap_class=ui-portlet,three,columns/>
-			<include:template name=Widgetsysteminfo wrap=div wrap_id=widget9 wrap_class=ui-portlet,three,columns/>
+			<include:template name=Widgetgraph wrap=div wrap_id=widget7 wrap_class=ui-portlet,four,columns/>
+			<include:template name=Widgetquicklinks wrap=div wrap_id=widget8 wrap_class=ui-portlet,four,columns/>
+			<include:template name=Widgetsysteminfo wrap=div wrap_id=widget9 wrap_class=ui-portlet,four,columns/>
 		</div>
 		<div wrap_class="row ui-portlet4">
-			<include:template name=Widgetgallery wrap=div wrap_id=widget10 wrap_class=ui-portlet,three,columns/>
-			<include:template name=Widgetlist wrap=div wrap_id=widget11 wrap_class=ui-portlet,three,columns/>
-			<include:template name=Widgettext wrap=div wrap_id=widget12 wrap_class=ui-portlet,three,columns/>
+			<include:template name=Widgetgallery wrap=div wrap_id=widget10 wrap_class=ui-portlet,four,columns/>
+			<include:template name=Widgetlist wrap=div wrap_id=widget11 wrap_class=ui-portlet,four,columns/>
+			<include:template name=Widgettext wrap=div wrap_id=widget12 wrap_class=ui-portlet,four,columns/>
 		</div>
 		<div wrap_class="row ui-portlet5">
-			<include:template name=Widgetaudio wrap=div wrap_id=widget13 wrap_class=ui-portlet,three,columns/>
-			<include:template name=Widgetvideo wrap=div wrap_id=widget14 wrap_class=ui-portlet,three,columns/>
-			<include:template name=Widgetmap wrap=div wrap_id=widget15 wrap_class=ui-portlet,three,columns/>
+			<include:template name=Widgetaudio wrap=div wrap_id=widget13 wrap_class=ui-portlet,four,columns/>
+			<include:template name=Widgetvideo wrap=div wrap_id=widget14 wrap_class=ui-portlet,four,columns/>
+			<include:template name=Widgetmap wrap=div wrap_id=widget15 wrap_class=ui-portlet,four,columns/>
 		</div>
 	</div>
 </div>
