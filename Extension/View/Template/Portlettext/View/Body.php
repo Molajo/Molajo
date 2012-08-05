1a43d0eae29bceac6388926600a9c945c0bc032e
AmyStephen <AmyStephen@gmail.com>
2012-08-05 00:30:14 -0500

    Portlet Text

--- Extension/View/Template/Portlettext/View/Body.php
+++ Extension/View/Template/Portlettext/View/Body.php
@@ -5,5 +5,5 @@
  * @copyright  2012 Individual Molajo Contributors. All rights reserved.
  * @license    GNU GPL v 2, or later and MIT, see License folder
  */
-defined('MOLAJO') or die; ?>
-<li><a href="#">Item</a></li>
+defined('MOLAJO') or die;
+echo html_entity_decode($this->row->criteria_text);     //todo: move this into the view
--- Extension/View/Template/Portlettext/View/Header.php
+++ Extension/View/Template/Portlettext/View/Header.php
@@ -6,8 +6,6 @@
  */
 defined('MOLAJO') or die; ?>
 <div class="ui-portlet-header">
-    <h4>Text</h4>
+	<h4><?php echo $this->row->criteria_title; ?></h4>
 </div>
 <div class="ui-portlet-content">
-	<p>Cupcake ipsum dolor. Sit amet bear claw I love tiramisu liquorice halvah applicake jelly. Sweet roll liquorice macaroon powder cheesecake tiramisu.</p>
-</div>
