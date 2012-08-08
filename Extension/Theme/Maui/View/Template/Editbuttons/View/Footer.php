7efe0b35573b74ec1977f52aaaeb50183ee74f2b
AmyStephen <AmyStephen@gmail.com>
2012-08-07 22:13:44 -0500

    Change Trigger to Plugin (except in JS)

--- Extension/Theme/Maui/View/Template/Editbuttons/View/Footer.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @package    Molajo
- * @copyright  2012 Amy Stephen. All rights reserved.
- * @license    GNU GPL v 2, or later and MIT, see License folder
- */
-defined('MOLAJO') or die; ?>
-</ul>
