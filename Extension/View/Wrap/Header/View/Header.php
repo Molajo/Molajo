5d17e0506e52f924d810aecca80a940145c80cd6
Amy Stephen <AmyStephen@gmail.org>
2012-08-12 17:19:48 -0500

    Split Views between Standard distribution (some will be removed as extensions) and Platform

--- Extension/View/Wrap/Header/View/Header.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @package    Molajo
- * @subpackage  Wrap
- * @copyright  Copyright (C)  2012 Individual Molajo Contributors. All rights reserved.
- * @license    GNU GPL v 2, or later and MIT, see License folder
- */
-defined('MOLAJO') or die;
-include EXTENSIONS_VIEWS . '/Wrap/Default/View/Header.php';
