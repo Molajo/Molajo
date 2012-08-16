795cedfc5dbe9c1e698c2c487a497fa0a72c6695
AmyStephen <AmyStephen@gmail.com>
2012-08-16 15:32:14 -0500

    Include for User Interface Objects

--- Includer/UIIncluder.php
+++ Includer/UIIncluder.php
@@ -6,6 +6,7 @@
  */
 namespace Molajo\Includer;
 
+use Molajo\Service\Services;
 use Molajo\Includer;
 
 defined('MOLAJO') or die;
@@ -17,6 +18,34 @@ defined('MOLAJO') or die;
  * @subpackage  Includer
  * @since       1.0
  */
-class UIIncluder extends Includer
+class UiIncluder extends Includer
 {
+
+	/**
+	 * process - render include statement for template configured for UI Object
+	 *
+	 * @param   $attributes <include:ui attr1=x attr2=y attr3=z ... />
+	 *
+	 * @return mixed 		<include:template attr1=x attr2=y attr3=z ... />
+	 * @since   1.0
+	 */
+	public function process($attributes = array())
+	{
+		/** Retrieve Attributes from the include statement */
+		$this->attributes = $attributes;
+		parent::getAttributes();
+
+		/** Retrieve Configuration option for User Interface Template */
+		$name = Services::Registry()->get('Configuration', 'ui-template-' . $this->name);
+		if (trim($name) == '' || $name === null) {
+			$name = 'ui-' . $this->name;
+		}
+
+		/** Create Include Template Statement and return as rendered output */
+		return '<include:template '
+			. ' name=' . $this->name
+			. explode(' ', $this->attributes)
+			. '/>';
+
+	}
 }
