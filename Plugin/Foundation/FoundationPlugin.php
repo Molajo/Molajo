24c2b153ecb0a210137334916583fc1d485a86ce
AmyStephen <AmyStephen@gmail.com>
2012-08-17 21:15:00 -0500

    Button Group UI Foundation

--- Plugin/Foundation/FoundationPlugin.php
+++ Plugin/Foundation/FoundationPlugin.php
@@ -28,6 +28,10 @@ class FoundationPlugin extends ContentPlugin
 	{
 		if (strtolower($this->get('template_view_path_node')) == 'ui-button-foundation') {
 			$this->button_general();
+
+		} elseif (strtolower($this->get('template_view_path_node')) == 'ui-button-group-foundation') {
+			$this->button_group();
+
 		} else {
 			return true;
 		}
@@ -36,7 +40,7 @@ class FoundationPlugin extends ContentPlugin
 	}
 
 	/**
-	 * Add CSS Class and ID to each row
+	 * Add CSS Class to each row for Button Options
 	 *
 	 * @return boolean
 	 * @since   1.0
@@ -52,7 +56,49 @@ class FoundationPlugin extends ContentPlugin
 		$button_class = trim($button_class) . ' ' . trim($button_size);
 		$button_class = trim($button_class) . ' ' . 'button';
 
-		$this->saveField(null, 'button_class', trim($button_class));
+		$button_class = ' class="' . htmlspecialchars(trim($button_class), ENT_NOQUOTES, 'UTF-8') . '"';
+
+		$this->saveField(null, 'button_class', $button_class);
+
+		return true;
+	}
+
+	/**
+	 * Add CSS Class to each row for Button Options
+	 *
+	 * @return boolean
+	 * @since   1.0
+	 */
+	public function button_group()
+	{
+		$button_type = $this->data->button_group_type;
+		$button_size = $this->data->button_group_size;
+		$button_shape = $this->data->button_group_shape;
+		$button_class = str_replace(',', ' ', $this->data->button_group_class);
+
+		$button_group_class = trim($button_type);
+		$button_group_class = trim($button_group_class) . ' ' . trim($button_shape);
+		$button_group_class = trim($button_group_class) . ' ' . trim($button_size);
+		$button_group_class = trim($button_group_class) . ' ' . trim($button_class);
+		$button_group_class = trim($button_group_class) . ' ' . 'button-group';
+
+		$button_group_class = ' class="' . htmlspecialchars(trim($button_group_class), ENT_NOQUOTES, 'UTF-8') . '"';
+
+		$this->saveField(null, 'button_group_class', $button_group_class);
+
+		$button_array = array();
+		$temp = explode('{{', $this->data->button_group_array);
+		foreach ($temp as $set) {
+			$set = str_replace(',', ' ', $set);
+			$set = str_replace(':', '=', $set);
+			$set = str_replace('}}', '', $set);
+			if (trim($set) == '') {
+			} else {
+				$button_array[] = trim($set);
+			}
+		}
+
+		$this->saveField(null, 'button_group_array', $button_array);
 
 		return true;
 	}
